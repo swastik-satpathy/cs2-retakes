a11fb06d9c7905451265d8f1bcb3ac546785887b
swastik-satpathy <swastiksatpathy@yahoo.com>
2026-03-16 04:26:30 +0530

    disabled certain plugins will work on them later, updated config.sh so it works I hope and updated the template too

--- modules/config.sh
+++ modules/config.sh
@@ -3,68 +3,37 @@
 echo "Configuring server files..."
 
 CFG_DIR="/home/cs2server/serverfiles/game/csgo/cfg"
-SERVER_CFG="$CFG_DIR/server.cfg"
+CS2SERVER_CFG="$CFG_DIR/cs2server.cfg"
+TEMPLATE_CFG="/home/cs2server/cs2-retakes/templates/cs2server.cfg"
 MAPCYCLE="/home/cs2server/serverfiles/game/csgo/mapcycle.txt"
 
 mkdir -p "$CFG_DIR"
 
 ########################################
-# server.cfg
+# cs2server.cfg from template
 ########################################
 
-declare -a SERVER_SETTINGS=(
-'hostname "{{SERVER_NAME}}"'
-'sv_tags "{{SERVER_TAGS}}"'
-'mp_autoteambalance 0'
-'mp_limitteams 0'
-'sv_cheats 0'
-'sv_lan 0'
-'maxplayers 10'
-)
-
-if [ ! -f "$SERVER_CFG" ]; then
-
-    echo "Creating server.cfg"
-
-    for line in "${SERVER_SETTINGS[@]}"; do
-        echo "$line" >> "$SERVER_CFG"
-    done
-
+if [ ! -f "$CS2SERVER_CFG" ]; then
+    echo "Copying template to cs2server.cfg"
+    cp "$TEMPLATE_CFG" "$CS2SERVER_CFG"
 else
-
-    echo "Merging server.cfg settings"
-
-    for line in "${SERVER_SETTINGS[@]}"; do
-
-        KEY=$(echo "$line" | awk '{print $1}')
-
-        if ! grep -q "^$KEY" "$SERVER_CFG"; then
-            echo "Adding missing setting: $KEY"
-            echo "$line" >> "$SERVER_CFG"
-        fi
-
-    done
-
+    echo "cs2server.cfg already exists. Skipping."
 fi
 
-
 ########################################
-# Replace template values
+# Replace template placeholders
 ########################################
 
-sed -i "s/{{SERVER_NAME}}/$SERVER_NAME/g" "$SERVER_CFG"
-sed -i "s/{{SERVER_TAGS}}/$SERVER_TAGS/g" "$SERVER_CFG"
-
+sed -i "s/{{SERVER_NAME}}/$SERVER_NAME/g" "$CS2SERVER_CFG"
+sed -i "s/{{SERVER_TAGS}}/$SERVER_TAGS/g" "$CS2SERVER_CFG"
 
 ########################################
 # mapcycle.txt
 ########################################
 
 if [ ! -f "$MAPCYCLE" ]; then
-
     echo "Creating mapcycle.txt"
-
-cat <<EOF > "$MAPCYCLE"
+    cat <<EOF > "$MAPCYCLE"
 de_mirage
 de_dust2
 de_inferno
@@ -74,16 +43,14 @@ de_anubis
 de_overpass
 de_vertigo
 EOF
-
 else
     echo "mapcycle.txt already exists. Skipping."
 fi
-
 
 ########################################
 # Permissions
 ########################################
 
-chown cs2server:cs2server "$SERVER_CFG" "$MAPCYCLE"
+chown cs2server:cs2server "$CS2SERVER_CFG" "$MAPCYCLE"
 
 echo "Server configuration complete."

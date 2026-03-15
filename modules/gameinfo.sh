4e28b6d30dcffa68d72f0fef6d9b2b72200fddfa
swastik-satpathy <swastiksatpathy@yahoo.com>
2026-03-16 04:12:52 +0530

    updating gameinfo.sh

--- modules/gameinfo.sh
+++ modules/gameinfo.sh
@@ -4,11 +4,13 @@ echo "Configuring gameinfo.gi..."
 
 GAMEINFO="/home/cs2server/serverfiles/game/csgo/gameinfo.gi"
 
+# Check if already configured
 if grep -q "csgo/addons/metamod" "$GAMEINFO"; then
     echo "Metamod already configured in gameinfo.gi"
-    return
+    exit 0
 fi
 
-sed -i '/Game_LowViolence csgo_lv/a\ \ \ \ Game csgo/addons/metamod' "$GAMEINFO"
+# Insert right after Game_LowViolence csgo_lv line with proper indentation
+sed -i '/^[[:space:]]*Game_LowViolence[[:space:]]\+csgo_lv/a\ \ \ \ Game csgo/addons/metamod' "$GAMEINFO"
 
 echo "Metamod entry added to gameinfo.gi"
